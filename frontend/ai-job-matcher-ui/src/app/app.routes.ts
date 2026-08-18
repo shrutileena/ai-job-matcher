24a62f95f38572e4e4c5368c5316e9855e265934
shruti agarwal <shrutileena@gmail.com>
2026-08-18 19:55:01 +0530

    frontend - login page

---
+++ frontend/ai-job-matcher-ui/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { Login } from './features/login/login';
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }
+];
